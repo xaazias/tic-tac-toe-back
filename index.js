159719eb00cc50fec79b7a0fb4a93e21acf10d49
xaazias <flamesuspect@gmail.com>
2021-05-26 01:50:09 +0300

    Added game check

--- index.js
+++ index.js
@@ -1,95 +1,167 @@
-const app = require('express')()
-const http = require('http').createServer(app)
+const app = require("express")()
+const http = require("http").createServer(app)
 
 /* socket.io WS server */
-const io = require('socket.io')(http, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 })
+
+/* Check win algorithm */
+const array_chunks = (array, chunk_size) =>
+  Array(Math.ceil(array.length / chunk_size))
+    .fill()
+    .map((_, index) => index * chunk_size)
+    .map((begin) => array.slice(begin, begin + chunk_size))
+
+function checkDiagonalsWin(char, arr, GRID_SIZE) {
+  arr = array_chunks(arr, GRID_SIZE)
+  let n = arr.length
+  let diag1 = []
+  let diag2 = []
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === j) {
+        diag1.push(arr[i][j])
+      }
+      if (i + j === n - 1) {
+        diag2.push(arr[i][j])
+      }
+    }
+  }
+
+  if (diag1.filter((item) => item.flag === char).length === GRID_SIZE)
+    return true
+
+  if (diag2.filter((item) => item.flag === char).length === GRID_SIZE)
+    return true
+
+  return false
+}
+
+const checkRowWin = (char, arr, GRID_SIZE) => {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    const temp = arr.filter((item) => item.row === i)
+    if (temp.filter((item) => item.flag === char).length === GRID_SIZE) {
+      return true
+    }
+  }
+  return false
+}
+
+const checkColumnWin = (char, arr, GRID_SIZE) => {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    const temp = arr.filter((item) => item.column === i)
+    if (temp.filter((item) => item.flag === char).length === GRID_SIZE) {
+      return true
+    }
+  }
+  return false
+}
+
+const checkWin = (char, arr) => {
+  const GRID_SIZE = Math.sqrt(arr.length)
+
+  if (checkRowWin(char, arr, GRID_SIZE)) return true
+  if (checkColumnWin(char, arr, GRID_SIZE)) return true
+  if (checkDiagonalsWin(char, arr, GRID_SIZE)) return true
+
+  return false
+}
 
 /* Game rooms e.g [{ name: GAME_ID, users: [USER_ID_1, USER_ID_2] }...] */
 const rooms = []
 
 /* WS connect event */
-io.on('connect', socket => {
-
+io.on("connect", (socket) => {
   /* WS "join" event with arguments of USER_ID and GAME_ROOM */
-  socket.on('join', ({ id, room }) => {
-
+  socket.on("join", ({ id, room }) => {
     /* Search if GAME_ROOM already exists ? else Create a new one */
-    if (rooms.findIndex(el => el.name === room) === -1)
+    if (rooms.findIndex((el) => el.name === room) === -1)
       rooms.push({ name: room, users: [id] })
 
     /* get GAME_ROOM index in 'rooms' array */
-    const roomIndex = rooms.findIndex(el => el.name === room)
+    const roomIndex = rooms.findIndex((el) => el.name === room)
 
     /* add USER to room if USERS < 2 in room and it's not the same USER */
-    if (rooms[roomIndex].users.length < 2 && !rooms[roomIndex].users.includes(id)) {
+    if (
+      rooms[roomIndex].users.length < 2 &&
+      !rooms[roomIndex].users.includes(id)
+    ) {
       /* join the room in WS */
-      socket.join(room) 
+      socket.join(room)
       /* push USER to 'rooms' array */
-      rooms[roomIndex].users.push(id) 
+      rooms[roomIndex].users.push(id)
     }
 
     /* if GAME_ROOM have 2 active users */
     if (rooms[roomIndex].users.length === 2) {
       /* WS message room with status */
-      io.to(room).emit('message', 'Игра в процессе')
-      
+      io.to(room).emit("message", "Игра в процессе")
+
       /* shuffle USERS in room's users-array */
       rooms[roomIndex].users.sort(() => Math.random() - 0.5)
-      
+
       /* WS emit random USER to make first turn */
-      io.to(rooms[roomIndex].users[0]).emit('turn', 1)
-      
+      io.to(rooms[roomIndex].users[0]).emit("turn", 1)
+
       /* WS emit room to be ready as players-connected */
-      io.to(room).emit('playersConnected', true)
+      io.to(room).emit("playersConnected", true)
     }
 
-    console.log(id, room, rooms) // log id, room, rooms on player 'connect' 
+    console.log(id, room, rooms) // log id, room, rooms on player 'connect'
   })
 
   /* WS on 'step' event */
-  socket.on('step', ({ room, grid, step }) => {
+  socket.on("step", ({ room, grid, step }) => {
     /* on each step emit current grid to room */
-    io.to(room).emit('step', { grid })
+    io.to(room).emit("step", { grid })
+
     /* on each step increment step number */
-    io.to(room).emit('nextStep', step + 1)
+    io.to(room).emit("nextStep", step + 1)
+
+    if (checkWin("x", grid)) 
+    io.to(room).emit("finishGame", { status: "x" })
+
+    if (checkWin("o", grid)) 
+      io.to(room).emit("finishGame", { status: "o" })
+
+    if (!checkWin("x", grid) && !checkWin("o", grid) && grid.length === step) 
+      io.to(room).emit("finishGame", { status: null })
   })
 
   /* WS on 'finishGame' event */
-  socket.on('finishGame', ({ id, room, status }) => {
+  socket.on("finishGame", ({ id, room, status }) => {
     /* emit to room as game finished */
     io.to(room).emit("finishGame", { id, status })
   })
-    
-  /* WS on 'disconnect' event */
-  socket.on('disconnect', () => {
 
+  /* WS on 'disconnect' event */
+  socket.on("disconnect", () => {
     /* get USERID */
     const id = socket.id
 
     /* search for user in all rooms */
-    rooms.forEach(room => {
+    rooms.forEach((room) => {
       /* get USER index in room's users array */
-      const index = room.users.findIndex(el => el === id)
-      
+      const index = room.users.findIndex((el) => el === id)
+
       /* if user is found */
       if (index !== -1) {
         /* delete USER from array */
         room.users.splice(index, 1)
         /* emit message to GAME_ROOM as not enough players for game to continue */
-        socket.to(room).emit('playersConnected', false)
+        socket.to(room).emit("playersConnected", false)
       }
     })
 
-    console.log(rooms) // log rooms on player 'disconnect' 
+    console.log(rooms) // log rooms on player 'disconnect'
   })
 })
 
 /* start server on port 4000 */
-http.listen(4000, function() {
-  console.log('listening on port 4000')
+http.listen(4000, function () {
+  console.log("listening on port 4000")
 })
---
+++ readme.md
@@ -0,0 +1,7 @@
+### How to create docker container
+
+`docker run --publish 4000:4000 test-back`
+
+### How to run docker container
+
+`docker run --publish 4000:4000 tic-back`
